88bbee1322db231ae915b030bfb96ae96982aac8
miracle3070 <miracle3070@naver.com>
2020-10-06 20:04:08 +0900

    회원가입 기능 구현 완료

--- Lotte_Logis/account/models.py
+++ Lotte_Logis/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=45)
+    phone = models.CharField(max_length=45)
+    permission = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
--- Lotte_Logis/account/views.py
+++ Lotte_Logis/account/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
+from .models import *
 
 # Create your views here.
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        user = User.objects.create_user(
+            username = request.POST['id'],
+            password = request.POST['password'],
+            email = request.POST['email']
+        )
+        profile = Profile(
+            user = user,
+            name = request.POST['name'],
+            phone = request.POST['phone'],
+            permission = int(request.POST['permission']),
+        )
+        profile.save()
+        return HttpResponse("회원가입 완료")        
+    else:
+        return render(request, 'signup.html')
+        
 
 def login(request):
     return render(request, 'login.html')
