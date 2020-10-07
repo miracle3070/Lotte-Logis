fea6cb78dd789078016809eadf5a79c4eaa5d04a
miracle3070 <miracle3070@naver.com>
2020-10-07 16:02:41 +0900

    로그인, 로그아웃 구현 / 회원가입 개선

--- Lotte_Logis/account/views.py
+++ Lotte_Logis/account/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 from .models import *
 
 # Create your views here.
@@ -17,10 +18,26 @@ def signup(request):
             permission = int(request.POST['permission']),
         )
         profile.save()
-        return HttpResponse("회원가입 완료")        
+        return render(request, 'complete_signup.html', { 'signup_user':user})       
     else:
         return render(request, 'signup.html')
         
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        username = request.POST['id']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html',
+            {'error': '아이디 또는 비밀번호가 일치하지 않습니다.'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
